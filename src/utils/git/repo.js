2671f208fc3785b938321df8b2c5f9b77fec3f2f
cqb325 <cqb325@163.com>
2019-01-17 14:57:58 +0800

    bug修改

--- src/utils/git/repo.js
+++ src/utils/git/repo.js
@@ -738,10 +738,10 @@ class Repo {
                 rets = unpushs.concat(upCommits);
 
                 rets.sort((a, b) => {
-                    if (a.time() < b.time()) {
+                    if (a.time < b.time) {
                         return 1;
                     }
-                    if (a.time() > b.time()) {
+                    if (a.time > b.time) {
                         return -1;
                     }
                     return 0;

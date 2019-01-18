ad8353202f772b999bf80517a446acec8308d26e
cqb325 <cqb325@163.com>
2019-01-18 22:51:37 +0800

    modify about

--- src/desktop/welcome/about.js
+++ src/desktop/welcome/about.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import electron from '../../images/electron.svg';
+import nodegit from '../../images/nodegit.svg';
 
 class AboutContent extends React.Component {
     displayName = 'AboutContent';
@@ -10,6 +12,14 @@ class AboutContent extends React.Component {
                 <img src={logo} style={{width: 64}}/>
                 <div>C-GIT (v0.1.0)</div>
                 <div>copyright &copy; cqb325@163.com</div>
+            </div>
+            <div className='mb-30 mt-30 text-center'>
+                <span>
+                    <img src={electron} style={{height: 50}}/>
+                </span>
+                <span className='ml-10'>
+                    <img src={nodegit} style={{height: 44}}/>
+                </span>
             </div>
         </div>;
     }
